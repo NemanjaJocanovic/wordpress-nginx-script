07ced48e99fb17c52024731db05e8b0262547f9e
NemanjaJocanovic <jocanovicnemanja@gmail.com>
2023-04-06 11:35:54 +0200

    Adding dependencies, database and downloading WordPress

--- wordpress_script.sh
+++ wordpress_script.sh
@@ -3,6 +3,21 @@
 #Update the System
 sudo apt update && sudo apt upgrade -y
 
+#Install dependencies
+if ! command -v wget &> /dev/null; then
+    echo "wget not found, installing..."
+    sudo apt install wget -y
+else
+    echo "wget is already installed"
+fi
+
+if ! command -v tar &> /dev/null; then
+    echo "tar not found, installing..."
+    sudo apt install tar -y
+else
+    echo "tar is already installed"
+fi
+
 #Install Nginx
 sudo apt install nginx -y
 sudo systemctl enable nginx
@@ -23,7 +38,6 @@ sudo chown -R $USER:$USER /var/www/$IP
 # Create a new Nginx configuration file
 sudo touch /etc/nginx/sites-available/$IP.conf
 
-# Nginx configuration file
 # Nginx configuration file
 cat <<EOF | sudo tee /etc/nginx/sites-available/$IP.conf
 server {
@@ -52,10 +66,6 @@ server {
 }
 EOF
 
-#Files for testing
-echo "<?php echo 'Hello, world!'; ?>" | sudo tee /var/www/$IP/index.php
-echo "<?php phpinfo(); ?>" | sudo tee /var/www/$IP/phpinfo.php
-
 #Enable Nginx configuration
 sudo ln -s /etc/nginx/sites-available/$IP.conf /etc/nginx/sites-enabled/
 
@@ -67,3 +77,33 @@ sudo nginx -t
 
 #Restart Nginx
 sudo systemctl restart nginx
+
+#Installing PHP extensions for WordPress
+sudo apt install php8.1-common php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-curl php8.1-gd php8.1-imagick php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring php8.1-opcache php8.1-soap php8.1-zip php8.1-intl -y
+
+#User Input for the database 
+read -p "Enter a name for the MySQL database: " DB_NAME
+read -p "Enter a username for the MySQL user: " DB_USER
+read -sp "Enter a password for the MySQL user: " DB_PASSWORD
+echo ""
+
+#Create a MySQL database
+sudo mysql -e "CREATE DATABASE $DB_NAME;"
+sudo mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+
+# Get the IP address of the instance
+IP=$(curl -s http://checkip.amazonaws.com)
+
+#Download latest WordPress package
+wget https://wordpress.org/latest.tar.gz
+
+#Extract the WordPress package
+tar -zxf latest.tar.gz
+
+#Copy WordPress files to the web root
+sudo cp -r wordpress/* /var/www/$IP
+
+#Setting ownership of the WordPress files
+sudo chown -R www-data:www-data /var/www/$IP
